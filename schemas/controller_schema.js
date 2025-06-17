7ee6c0528f01b4db1c642bfd2888c0c16edc2252
deslinc <deslinc19@gmail.com>
2025-06-17 19:43:11 +0000

    validated

---
+++ schemas/controller_schema.js
@@ -0,0 +1,8 @@
+import Joi from "joi";
+export const propertiesSchema = Joi.object({
+    name: Joi.string().required(),
+    address: Joi.string().required(),
+    type: Joi.string(),
+    description: Joi.string(),
+    image: Joi.string()
+})
