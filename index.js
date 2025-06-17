161b378b55287f2a0d06e4110de5b38679618c07
Bertojnr <snrdeveloper07@gmail.com>
2025-06-17 16:10:50 +0000

    added payment model controller and routes

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ import express from "express"
 import cors from "cors"
 import mongoose from "mongoose"
 import 'dotenv/config'
-import { userRouter } from "./routes/user_routes.js"
+// import { userRouter } from "./routes/user_routes.js"
+import { paymentRouter } from "./routes/payment_routes.js"
 
 
 const app = express()
+app.use(express.json());
 
 const PORT = process.env.PORT || 7508
 
@@ -17,11 +19,11 @@ export const secret = process.env.JWT_SECRET
 
 // export const SMTP_PASS = process.env.SMTP_PASS
 
-app.use(express.json())
 
-app.use(cors());
 
-app.use('/api/v1/users', userRouter)
+ 
+// app.use('/api/v1/users', userRouter)
+app.use('/api/v1', paymentRouter)
 
 
 await mongoose.connect(mongoURI)
