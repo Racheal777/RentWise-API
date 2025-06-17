4ad1d036ca224b9b19a5002d13a392f10d750e5c
kofi-lartey <kofilartey12@gmail,com>
2025-06-17 22:34:05 +0000

    Add Joi validation schema for user registration

---
+++ schemas/user_schema.js
@@ -0,0 +1,9 @@
+import Joi from "joi";
+
+export const userSchema = Joi.object({
+    firstName: Joi.string().min(3).max(30).required(),
+    lastName: Joi.string().min(3).max(30).required(),
+    email: Joi.string().email().required(),
+    password: Joi.string().min(6).max(1024).required(),
+    role: Joi.string().valid('admin', 'tenant').default('tenant')
+})
