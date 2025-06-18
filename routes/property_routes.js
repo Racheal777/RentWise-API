23539dcf6db49d908946d49e0aefc1920e7f333b
deslinc <deslinc19@gmail.com>
2025-06-18 10:05:45 +0000

    update

--- routes/property_routes.js
+++ routes/property_routes.js
@@ -1,11 +1,14 @@
 import { Router } from 'express';
-import { createProperty,getAllProperties,getPropertyById,patchProperty,deleteProperty } from '../controllers/property_controller.js';
+import { createProperty,getAllProperties,getPropertyById,patchProperty,deleteProperty, getMyProperties, deleteMyProperty } from '../controllers/property_controller.js';
 import { authenticate } from '../middlewares/auth.js';
+import { upload } from '../middlewares/image_upload.js';
 
 export const PropertyRouter = Router()
 
-PropertyRouter.post('/', authenticate, createProperty); 
-PropertyRouter.get('/', getAllProperties);       
+PropertyRouter.post('/', authenticate, upload.single('image'), createProperty); 
+PropertyRouter.get('/', getAllProperties); 
+PropertyRouter.get('/my/properties', getMyProperties);      
 PropertyRouter.get('/:id', getPropertyById);            
-PropertyRouter.patch('/:id', patchProperty);             
+PropertyRouter.patch('/:id', patchProperty); 
+PropertyRouter.delete('/:id', authenticate, deleteMyProperty);            
 PropertyRouter.delete('/:id', deleteProperty);          
