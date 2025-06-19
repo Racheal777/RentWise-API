9e17ee8685f89f2aec8b30491b2336af8b6ffbbd
kaagyebi <kaagyebi@gmail.com>
2025-06-19 05:39:14 -0700

    added route for

--- routes/maintenance_routes.js
+++ routes/maintenance_routes.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { upload } from "../middlewares/maintenance_middleware.js";
-import {createMaintenanceRequest, getAllMaintenanceRequests, updateMaintenanceStatus} from "../controllers/maintenance_controllers.js";
+import { createMaintenanceRequest, getAllMaintenanceRequests, updateMaintenanceStatus, getTenantMaintenanceRequests } from "../controllers/maintenance_controllers.js";
 import { authenticate } from '../middlewares/auth.js';
 
 
@@ -8,6 +8,11 @@ export const maintenanceRouter = Router()
 
 // Tenant creates a maintenance request
 maintenanceRouter.post("/maintenance", upload.array("images", 5), authenticate, createMaintenanceRequest);
+
+//Tenant view their requests
+maintenanceRouter.get(
+    "/maintenance/my-requests",authenticate, getTenantMaintenanceRequests
+);
 
 // Admin views all maintenance requests
 maintenanceRouter.get("/maintenance", authenticate, getAllMaintenanceRequests);
